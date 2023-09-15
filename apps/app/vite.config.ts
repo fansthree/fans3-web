ae6623ced0972939ff03c96f910d401cc803574e
Jay <>
2023-09-15 20:56:07 +0800

    do not empty build dir for pushing to github pages

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,5 +1,10 @@
 import { viteConfig } from '@fans3/ui/src/shared/vite.config.cjs'
 
 export default ({ mode = '' }) => {
-  return viteConfig({ server: { port: 3000 } })({ mode })
+  return viteConfig({
+    server: { port: 3000 },
+    build: {
+      emptyOutDir: false
+    }
+  })({ mode })
 }
